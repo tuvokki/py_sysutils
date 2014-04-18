ca81ea4431d5ddf9b2512a53ab6222210ad72cd2
Wouter <tuvokki@gmail.com>
2014-04-18 18:48:11 +0200

    First version of the new_repo script

---
+++ new_git_repo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+##
+# This is what we have to do
+##
+# cd /home/development/git
+# sudo mkdir test.git
+# cd test.git
+# sudo git --bare init
+# cd ..
+# sudo chown -R git:git test.git
+##
+print "Nothing implemented yet. This is what we want to do, eventually:"
+
+print "cd /home/development/git\n"
+print "sudo mkdir test.git\n"
+print "cd test.git\n"
+print "sudo git --bare init\n"
+print "cd ..\n"
+print "sudo chown -R git:git test.git\n"
+
