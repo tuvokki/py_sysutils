bd3d56eef300b181c4b746a7ad2d74c8eb0f3e05
Wouter <tuvokki@gmail.com>
2014-04-18 22:13:45 +0200

    Added help message and shorthand for reponame argument

--- new_git_repo.py
+++ new_git_repo.py
@@ -2,28 +2,19 @@
 
 import argparse, os, subprocess
 
-parser = argparse.ArgumentParser(description='Create a new git repo.')
+parser = argparse.ArgumentParser(description='Create a new git repo.',epilog='Run this program as regular user with sudo.')
 
-parser.add_argument('-repo_name', help='The name of the repo', required=True)
+parser.add_argument('-r','--repo_name', help='The name of the repo', required=True)
 
 args = parser.parse_args()
 
-#git_dir = "/Users/wouter/test/development/git/"
-git_dir = "/home/development/git/"
+git_dir = "/Users/wouter/test/development/git/"
+git_user = "root"
+git_group = "wheel"
+# git_dir = "/home/development/git/"
+# git_user = "git"
+# git_group = "git"
 repo_dir = git_dir + args.repo_name + ".git"
-git_user = "git"
-git_group = "git"
-##
-# This is what we have to do
-##
-# cd /home/development/git
-# sudo mkdir test.git
-# cd test.git
-# sudo git --bare init
-# cd ..
-# sudo chown -R git:git test.git
-##
-print "Nothing implemented yet. This is what we want to do, eventually:"
 
 # create git_dir
 if not os.path.isdir(repo_dir):
@@ -37,6 +28,17 @@ if not os.path.isdir(repo_dir):
     # print "cd .."
     # print "sudo chown -R git:git %s.git" %(args.repo_name)
     proc = subprocess.Popen(['/usr/bin/env','chown','-R',git_user+':'+git_group,repo_dir])
+    print """
+  # Now on the client's computer
+  $ cd myproject
+  $ git init
+  $ git add .
+  $ git commit -m 'initial commit'
+  $ git remote add origin git@gitserver:/opt/git/project.git
+  $ git push origin master
+  # to get the repo's updates
+  $ git pull origin master
+    """
   except OSError, e:
     raise e
 
