41e315b88f6da1f606d76845efbfc0b2d99a28d6
Wouter <tuvokki@gmail.com>
2014-04-18 20:53:40 +0200

    create git dir

--- new_git_repo.py
+++ new_git_repo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse
+import argparse, os, subprocess
 
 parser = argparse.ArgumentParser(description='Create a new git repo.')
 
@@ -8,7 +8,8 @@ parser.add_argument('-repo_name', help='The name of the repo', required=True)
 
 args = parser.parse_args()
 
-git_dir = "/home/development/git/"
+git_dir = "/Users/wouter/test/development/git/"
+#git_dir = "/home/development/git/"
 
 ##
 # This is what we have to do
@@ -21,6 +22,9 @@ git_dir = "/home/development/git/"
 # sudo chown -R git:git test.git
 ##
 print "Nothing implemented yet. This is what we want to do, eventually:"
+
+# create git_dir
+os.makedirs(git_dir + args.repo_name + ".git")
 
 print "cd %s" %(args.repo_name)
 print "sudo mkdir %s.git" %(args.repo_name)
