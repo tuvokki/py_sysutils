0481efb10a9ad1dcca5d20f13c9152b4e8d92485
Wouter <tuvokki@gmail.com>
2014-04-19 09:32:16 +0200

    Added defaults and some comments

--- new_git_repo.py
+++ new_git_repo.py
@@ -5,42 +5,46 @@ import argparse, os, subprocess
 parser = argparse.ArgumentParser(description='Create a new git repo.',epilog='Run this program as regular user with sudo.')
 
 parser.add_argument('-r','--repo_name', help='The name of the repo', required=True)
+parser.add_argument('-d','--git_dir', help='The base dir of git', dest='git_dir', default='/home/development/git/', required=False)
+parser.add_argument('-u','--git_user', help='The name git user', dest='git_user', default='git', required=False)
+parser.add_argument('-g','--git_group', help='The groups of the git user', dest='git_group', default='git', required=False)
 
 args = parser.parse_args()
 
-git_dir = "/Users/wouter/test/development/git/"
-git_user = "root"
-git_group = "wheel"
-# git_dir = "/home/development/git/"
-# git_user = "git"
-# git_group = "git"
-repo_dir = git_dir + args.repo_name + ".git"
+repo_dir = args.git_dir + args.repo_name + ".git"
+repo_rights = args.git_user+':'+args.git_group
 
 # create git_dir
 if not os.path.isdir(repo_dir):
   try:
-    # print "cd %s" %(args.repo_name)
-    # print "sudo mkdir %s.git" %(args.repo_name)
+
+    # Create the repository directory
     os.makedirs(repo_dir)
-    # print "cd %s.git" %(args.repo_name)
-    # print "sudo git --bare init"
+
+    # Initialize the repor with: git --bare init
     proc = subprocess.Popen(['/usr/bin/env','git','--bare','init'], cwd=repo_dir)
-    # print "cd .."
-    # print "sudo chown -R git:git %s.git" %(args.repo_name)
-    proc = subprocess.Popen(['/usr/bin/env','chown','-R',git_user+':'+git_group,repo_dir])
+
+    print "Adding rights [%s] to repo"%(repo_rights)
+    # chown -R git:git repo_name
+    proc = subprocess.Popen(['/usr/bin/env','chown','-R',repo_rights,repo_dir])
+
     print """
   # Now on the client's computer
   $ cd myproject
   $ git init
   $ git add .
   $ git commit -m 'initial commit'
-  $ git remote add origin git@gitserver:/opt/git/project.git
+  $ git remote add origin git@gitserver:%s
   $ git push origin master
   # to get the repo's updates
   $ git pull origin master
-    """
+    """ % (repo_dir)
   except OSError, e:
     raise e
-
-
+  except IOError as e:
+      if (e[0] == errno.EPERM):
+         print >> sys.stderr, "You need root permissions to do this, laterz!"
+         sys.exit(1)
+else:
+  print "%s already exists! Aborting!"%(repo_dir)
 
