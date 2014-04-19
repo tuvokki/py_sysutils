5ac05b2558dbf945771e087852e91a514239aaed
Wouter <tuvokki@gmail.com>
2014-04-19 10:16:12 +0200

    Changed Popen to call. Error checking on the call commands. moved last message to seperate method.

--- new_git_repo.py
+++ new_git_repo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-import argparse, os, subprocess
+import argparse, os, subprocess, sys
+
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
 parser = argparse.ArgumentParser(description='Create a new git repo.',epilog='Run this program as regular user with sudo.')
 
@@ -8,6 +11,19 @@ parser.add_argument('-r','--repo_name', help='The name of the repo', required=Tr
 parser.add_argument('-d','--git_dir', help='The base dir of git', dest='git_dir', default='/home/development/git/', required=False)
 parser.add_argument('-u','--git_user', help='The name git user', dest='git_user', default='git', required=False)
 parser.add_argument('-g','--git_group', help='The groups of the git user', dest='git_group', default='git', required=False)
+
+def show_next(rd):
+  print """
+# Now on the client's computer
+$ cd myproject
+$ git init
+$ git add .
+$ git commit -m 'initial commit'
+$ git remote add origin git@gitserver:%s
+$ git push origin master
+# to get the repo's updates
+$ git pull origin master
+  """ % (rd)
 
 args = parser.parse_args()
 
@@ -22,29 +38,22 @@ if not os.path.isdir(repo_dir):
     os.makedirs(repo_dir)
 
     # Initialize the repor with: git --bare init
-    proc = subprocess.Popen(['/usr/bin/env','git','--bare','init'], cwd=repo_dir)
+    git_init_prog = ['/usr/bin/env','git','--bare','init']
+    subprocess.call(git_init_prog, cwd=repo_dir)
 
     print "Adding rights [%s] to repo"%(repo_rights)
     # chown -R git:git repo_name
-    proc = subprocess.Popen(['/usr/bin/env','chown','-R',repo_rights,repo_dir])
+    chown_proc = ['/usr/bin/env','chown','-R',repo_rights,repo_dir]
+    try:
+      subprocess.check_call(chown_proc)
+    except subprocess.CalledProcessError, e:
+      print "Process error(%s)\nUser %s or group %s does not exist on this system. Exiting."%(e.returncode, args.git_user, args.git_group)
+      sys.exit(1)
 
-    print """
-  # Now on the client's computer
-  $ cd myproject
-  $ git init
-  $ git add .
-  $ git commit -m 'initial commit'
-  $ git remote add origin git@gitserver:%s
-  $ git push origin master
-  # to get the repo's updates
-  $ git pull origin master
-    """ % (repo_dir)
+    show_next(repo_dir)
   except OSError, e:
-    raise e
-  except IOError as e:
-      if (e[0] == errno.EPERM):
-         print >> sys.stderr, "You need root permissions to do this, laterz!"
-         sys.exit(1)
+    print "I/O error(%s): %s\nMaybe try a different git-base directory with the --git_dir option. Exiting."%(e.errno, e.strerror)
+    sys.exit(1)
 else:
   print "%s already exists! Aborting!"%(repo_dir)
 
